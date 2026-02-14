7e97294e3586af2972e61fdcff9b2b8d2464e520
OKADA Shohei <okashoicircus0@gmail.com>
2026-02-14 19:34:37 +0900

    PHP-CS-Fixer 設定

--- backend/src/.php-cs-fixer.dist.php
+++ backend/src/.php-cs-fixer.dist.php
@@ -1,13 +1,31 @@
 <?php
 
-$finder = (new PhpCsFixer\Finder())
+$finder = new PhpCsFixer\Finder()
     ->in(__DIR__)
-    ->exclude('var')
+    ->exclude([
+        'config',
+        'var',
+    ])
+    ->notPath([
+        'public/index.php',
+        'tests/bootstrap.php',
+    ])
 ;
 
-return (new PhpCsFixer\Config())
+return new PhpCsFixer\Config()
     ->setRules([
+        '@PSR12' => true,
         '@Symfony' => true,
+        'phpdoc_summary' => false,
+        'yoda_style' => false,
+        'concat_space' => ['spacing' => 'one'],
+        'phpdoc_separation' => [
+            'groups' => [['param', 'return', 'throws']],
+        ],
+        'phpdoc_align' => ['align' => 'left'],
+        'no_superfluous_phpdoc_tags' => ['remove_inheritdoc' => false],
+        'php_unit_method_casing' => false,
     ])
     ->setFinder($finder)
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
 ;
