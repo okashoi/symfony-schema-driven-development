c18fbd92bd6c2f4c8b35accd026b807297879485
OKADA Shohei <okashoicircus0@gmail.com>
2026-02-14 23:23:51 +0900

    Jest 設定

---
+++ frontend/jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+};
+
+export default createJestConfig(config);
