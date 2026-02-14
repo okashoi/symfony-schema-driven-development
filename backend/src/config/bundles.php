2efc803aae8509bd645b7d25cfc6d0998cda8654
OKADA Shohei <okashoicircus0@gmail.com>
2026-02-14 21:29:22 +0900

    NelmioApiDocBundle インストール

--- backend/src/config/bundles.php
+++ backend/src/config/bundles.php
@@ -3,4 +3,5 @@
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Nelmio\ApiDocBundle\NelmioApiDocBundle::class => ['all' => true],
 ];
